3fa9fca4113fd213ce4cdd4777f422b9b46cb069
reza76 <reza76py@gmail.com>
2024-04-06 13:03:34 +1000

    Giving Data with Selecting Days

--- src/App.js
+++ src/App.js
@@ -4,22 +4,36 @@ import * as XLSL from 'xlsx'
 
 function App() {
     const [data, setData] = useState([])
+    const [selectDay, setSelectDay] = useState("Monday")
     useEffect(()=>{
         fetch('ssaReport.xlsx')
             .then((res)=>res.arrayBuffer())
             .then((buffer)=>{
                 const workbook = XLSL.read(buffer, {type: "buffer"});
-                const worksheetName = workbook.SheetNames[4];
-                const worksheet = workbook.Sheets[worksheetName];
+
+                const worksheet = workbook.Sheets[selectDay];
                 const jsonData = XLSL.utils.sheet_to_json(worksheet);
                 setData(jsonData);
 
             })
-    }, [])
+    }, [selectDay])
+
+    const hadleDayCheng = (event) => {
+        setSelectDay(event.target.value);
+        setData([])
+    }
 
     return (
         <div className="App">
             <h1>Excel Data</h1>
+            <select value={selectDay} onChange={hadleDayCheng}>
+                <option>Monday</option>
+                <option>Tuesday</option>
+                <option>Wednesday</option>
+                <option>Thursday</option>
+                <option>Friday</option>
+
+            </select>
             <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
     )
